9817b71dccecde07f038e01b00aae809b01a471d
Vinicius Cordeiro <cerradogstudio-viniciuscordeiro@gmail.com>
2026-08-29 20:28:43 -0300

    add backend structure with initial files and configuration

---
+++ backend/config/config.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * @file index.php
+ * @brief Entrypoint for the backend
+ * @author Vinicius Goncalves Cordeiro <vinicordeirogo@gmail.com><https://github.com/vinicius-g-cordeiro>
+ * @version 1.0
+ * @date 2026/08/29
+ * @copyright Copyright (c) 2026 - Vinicius Goncalves Cordeiro <vinicordeirogo@gmail.com> <https://github.com/vinicius-g-cordeiro>
+ */
+
+declare(strict_types=1);
+
---
+++ backend/public/index.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * @file index.php
+ * @brief Entrypoint for the backend
+ * @author Vinicius Goncalves Cordeiro <vinicordeirogo@gmail.com><https://github.com/vinicius-g-cordeiro>
+ * @version 1.0
+ * @date 2026/08/29
+ * @copyright Copyright (c) 2026 - Vinicius Goncalves Cordeiro <vinicordeirogo@gmail.com> <https://github.com/vinicius-g-cordeiro>
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+header('Content-Type: application/json');
+http_response_code(200);
+echo json_encode([
+    'status' => 'ok',
+    'success' => true,
+    'code' => 200,
+    'data' => []
+]);
+
+exit(0);
